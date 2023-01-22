fc58b550c2d854dc1f5a5eed5305890f5af5cb11
BalaPriyan <110181533+GoodLove1@users.noreply.github.com>
2023-01-22 07:40:48 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -82,4 +82,4 @@ class TelegraphHelper:
             LOGGER.error(f'Failed Revoking telegraph access token due to : {e}')
 
 
-telegraph=TelegraphHelper('Jmdkh-mltb', 'https://github.com/junedkh/jmdkh-mltb')
+telegraph=TelegraphHelper('newkingmltb', 'https://github.com/GoodLove1/newkingmltb')
