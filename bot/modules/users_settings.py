4af63a4b9c45c52f5467fa4777d55f5af79135e3
BalaPriyan <110181533+GoodLove1@users.noreply.github.com>
2023-01-22 08:11:03 +0530

    Update users_settings.py

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -245,13 +245,13 @@ Check all available qualities options <a href="https://github.com/yt-dlp/yt-dlp#
         rmsg = f'''
 Send Leech Prefix. Timeout: 60 sec
 Examples:
-1. <code>{escape('<b>@JMDKH_Team</b>')}</code> 
+1. <code>{escape('<b>@fondness_mov</b>')}</code> 
 This will give output of:
-<b>@JMDKH_Team</b>  <code>50MB.bin</code>.
+<b>@fondness_mov</b>  <code>50MB.bin</code>.
 
-2. <code>{escape('<code>@JMDKH_Team</code>')}</code> 
+2. <code>{escape('<code>@fondness_mov</code>')}</code> 
 This will give output of:
-<code>@JMDKH_Team</code> <code>50MB.bin</code>.
+<code>@fondness_mov</code> <code>50MB.bin</code>.
 
 Check all available formatting options <a href="https://core.telegram.org/bots/api#formatting-options">HERE</a>.
         '''
